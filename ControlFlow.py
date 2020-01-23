4dbbe5de07ee9c68c5a37cfb46e7af13875c25be
Brady Carriveau <02brady.carriveau@gulllakecs.org>
2020-01-23 08:27:37 -0500

    add Double for loop

---
+++ ControlFlow.py
@@ -0,0 +1,10 @@
+
+# programmer Brady Carriveau
+#Date: 1.20.20
+# progam: Double For Loop
+
+
+for i in range(3):
+    print("Outer For Loop" + str(i))
+    for k in range(4):
+        print("\tInner for loop" + str(k))
