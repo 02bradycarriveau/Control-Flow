f5baf2f003c9c06c0c7789782529cdc901437336
Brady Carriveau <02brady.carriveau@gulllakecs.org>
2020-02-03 08:35:00 -0500

    Add Greeting

---
+++ ControlFlow.py
@@ -0,0 +1,15 @@
+# Date: 2.3.20
+# Greeting Function
+
+name = input("What is your name:  ")
+
+# Create Functions Here
+
+def greeting():
+    print("Hi there "   +   name + "!")
+    print("Very Nice to met you " + name)
+
+
+# Call Functions Here
+
+greeting()
